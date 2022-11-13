394839839b4abc9ce70e597719874afaf2956c3d
Jonathan <jonathansilva1938@hotmail.comm>
2022-11-13 11:48:13 -0300

    create models

--- page/models.py
+++ page/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailNewsletter(models.Model):
+    email = models.CharField(max_length=200, unique=True)
+    registred_at = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.email + self.registred_at
