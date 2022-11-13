6ffff00eeb107b96390bdf3525d165d592efce59
Jonathan <jonathansilva1938@hotmail.comm>
2022-11-13 12:57:22 -0300

    configure admin and settings

--- page/models.py
+++ page/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class EmailNewsletter(models.Model):
     email = models.CharField(max_length=200, unique=True)
     registred_at = models.DateTimeField('date published')
-
     def __str__(self):
-        return self.email + self.registred_at
+        return self.email + ' ' + str(self.registred_at)
+      
